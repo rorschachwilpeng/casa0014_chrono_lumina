4f3059db5b3c601e15b556fbc988d392c173c843
rorschachwilpeng <slimrorschach@outlook.com>
2025-01-06 19:42:09 +0000

    sry for uploaded the secrect accidentally:(

--- src/dev/new_dev_sketch/arduino_secrets.h
+++
@@ -1,4 +0,0 @@
-#define SECRET_SSID "CE-Hub-Student"      // wifi name
-#define SECRET_PASS "casa-ce-gagarin-public-service"  // wifi pwd
-#define SECRET_MQTTUSER "student"  // mqtt user name
-#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"  // mqtt pwd
